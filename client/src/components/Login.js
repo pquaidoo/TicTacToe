dcde26ef89c1948cb9ac111997d450159b9a7454
Jayana Estacio <jpstacio@uw.edu>
2024-02-07 21:18:40 -0800

    basic sign-up and login buttons

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const login = () => {};
     return(
-        <div>Login</div>
-    )
+        <div className = "login">
+        <label>Login</label>
+            
+        <input
+            placeholder ="Username"
+            onChange={(event) => {
+                setUsername(event.target.value);
+            }}
+        /> 
+        <input
+            placeholder ="Password"
+            type = "password"
+            onChange={(event) => {
+                setPassword(event.target.value);
+            }}
+        /> 
+            <button onClick = {login}>Login</button>
+    </div>
+    );
 }
 
 export default Login;
-/*hello*/
