eea448cc66669c1b739de777d6c0fccc7c978b22
Jayana Estacio <jpstacio@uw.edu>
2024-02-08 21:20:44 -0800

    log in/out works and creating new users and logging back in

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,27 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 import { StreamChat } from 'stream-chat';
 import Cookies from 'universal-cookie';
+import { useState } from 'react';
 
 function App() {
   const api_key = "gzjne9xnnc5j";
   const cookies = new Cookies();
   const token = cookies.get("token");
   const client = StreamChat.getInstance(api_key);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("hashedPassword");
+    cookies.remove("channelName");
+    cookies.remove("username");
+    client.disconnectUser();
+    setIsAuth(false);
+  }
+
 
   if (token) {
     client.connectUser ({
@@ -21,13 +36,17 @@ function App() {
       token
     )
     .then((user) => {
-      console.log(user)
+      setIsAuth(true);
     });
   }
   return (
     <div className="App">
-      <SignUp />
-      <Login />
+      {isAuth ? (<button onClick = {logOut}> Log Out </button>) : (
+        <>
+        <SignUp setIsAuth = {setIsAuth}/>
+        <Login setIsAuth = {setIsAuth}/>
+        </>
+      )}
     </div>
   );
 }
