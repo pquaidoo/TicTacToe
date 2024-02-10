73529336acece33aa915e6b636e4eea9241ee1c1
Jayana Estacio <jpstacio@uw.edu>
2024-02-09 20:07:11 -0800

    game works!

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Board from "./Board";
+import { Window, MessageList, MessageInput } from "stream-chat-react";
+import "./Chat.css";
+function Game({ channel, setChannel }) {
+  const [playersJoined, setPlayersJoined] = useState(
+    channel.state.watcher_count === 2
+  );
+  const [result, setResult] = useState({ winner: "none", state: "none" });
 
-function Game({ channel }) {
-    const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2);
-
-    channel.on("user.watching.start", (event) => {
-        setPlayersJoined(event.watcher_count === 2)
-    });
-    if (!playersJoined) {
-        return <div>Waiting for other player to join...</div>
-    }
-    return <div>Game</div>;
+  channel.on("user.watching.start", (event) => {
+    setPlayersJoined(event.watcher_count === 2);
+  });
+  if (!playersJoined) {
+    return <div> Waiting for other player to join...</div>;
+  }
+  return (
+    <div className="gameContainer">
+      <Board result={result} setResult={setResult} />
+      <Window>
+        <MessageList
+          disableDateSeparator
+          closeReactionSelectorOnClick
+          hideDeletedMessages
+          messageActions={["react"]}
+        />
+        <MessageInput noFiles />
+      </Window>
+      <button
+        onClick={async () => {
+          await channel.stopWatching();
+          setChannel(null);
+        }}
+      >
+        {" "}
+        Leave Game
+      </button>
+      {result.state === "won" && <div> {result.winner} Won The Game</div>}
+      {result.state === "tie" && <div> Game Tieds</div>}
+    </div>
+  );
 }
 
 export default Game;
